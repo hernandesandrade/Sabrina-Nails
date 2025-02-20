f27f489ae8f982622cad1eb1c5284fa657ff79ba
Hernandes Andrade <hernandesandrade12@gmail.com>
2025-02-20 12:01:30 -0300

    cores aplicadas no log de movimentaçoes

--- src/main/java/com/online/commerce/controllers/gestao/produtos/MovimentacaoController.java
+++ src/main/java/com/online/commerce/controllers/gestao/produtos/MovimentacaoController.java
@@ -2,16 +2,19 @@ package com.online.commerce.controllers.gestao.produtos;
 
 import com.online.commerce.models.Movimentacao;
 import com.online.commerce.models.Produto;
+import com.online.commerce.repositories.MovimentacaoRepository;
 import com.online.commerce.services.MovimentacaoService;
 import com.online.commerce.services.ProdutoService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/movimentacao")
+@RequestMapping("/gestao-produtos/movimentacao")
 @Controller
 public class MovimentacaoController {
 
@@ -21,8 +24,17 @@ public class MovimentacaoController {
     @Autowired
     MovimentacaoService movimentacaoService;
 
+    @Autowired
+    MovimentacaoRepository movimentacaoRepository;
+
     @GetMapping
-    public String view(Model model){
+    public String view(@RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "10") int size,
+                       Model model){
+        Page<Movimentacao> movimentacoes = movimentacaoRepository.findAll(PageRequest.of(page, size));
+
+        // Adiciona a lista de movimentações ao modelo
+        model.addAttribute("movimentacoes", movimentacoes);
         model.addAttribute("movimentacao", new Movimentacao());
         model.addAttribute("movimentacoes", movimentacaoService.getMovimentacoes());
         return "gestao/produtos/movimentacao";
@@ -43,7 +55,7 @@ public class MovimentacaoController {
         String mensagem = retorno.split("%")[1];
         model.addAttribute("mensagem", mensagem);
         model.addAttribute("estado", confirmacao);
-        return "redirect:/movimentacao";
+        return "redirect:/gestao-produtos/movimentacao";
     }
 
 
